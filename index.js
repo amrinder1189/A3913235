3d3b33baf4dae5adfb03b7189842591d56c3c54f
Amrinder Singh <59329623+amrinder1189@users.noreply.github.com>
2024-11-12 09:53:26 -0500

    codee

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
+// Middleware ghgh
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
